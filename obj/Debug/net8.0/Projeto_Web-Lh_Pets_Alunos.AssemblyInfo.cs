965bfe02bff3016d53d9d9784d65baafb3988a0f
iz-Brum <izann.brum@alunosenai.mt>
2024-04-16 09:29:35 -0400

    Atualização do README.md

--- obj/Debug/net8.0/Projeto_Web-Lh_Pets_Alunos.AssemblyInfo.cs
+++ obj/Debug/net8.0/Projeto_Web-Lh_Pets_Alunos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_Web-Lh_Pets_Alunos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c549f780881165b39d457001ee0f66fc9bf196d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto_Web-Lh_Pets_Alunos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto_Web-Lh_Pets_Alunos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
